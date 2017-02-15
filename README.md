3b400f3f95dbe57c4414ba13e8b667c07597f380
Ricardo Fontanelli <ricardo.fontanelli@hotmail.com>
2017-02-15 01:17:12 -0200

    CHANGE improve readme information

--- README.md
+++ README.md
@@ -4,11 +4,14 @@ A simple and lightweight Laravel 4.2 and Laravel 5.* wrapper to interact with Te
 ## Get Started:
 * First of all, you should create a Telegram Bot, you can use [Bot Father](https://core.telegram.org/bots#6-botfather) to do that;
 * Create a Telegram chat room (group) and add the Bot to this group, now the Bot can send messages!
-* After publish the package, open the ```config/packages/ricardofontanelli/laravel-telegram/config.php``` and provide all the needed information.
+* After publish the package, open the telegram config file and provide all the required information.
 
 ## Installation
-The Laravel Telegram Service Provider can be installed via [Composer](http://getcomposer.org) by requiring the `ricardofontanelli/laravel-telegram` package in your project's `composer.json`.
-
+The Laravel Telegram Service Provider can be installed via ...
+```sh 
+composer require ricardofontanelli/laravel-telegram:1.0 
+```
+ or [Composer](http://getcomposer.org) by requiring the `ricardofontanelli/laravel-telegram` package in your project's `composer.json`
 ```json
 {
     "require": {
@@ -42,17 +45,48 @@ Find the `aliases` key in your `config/app.php` and add the Laravel Telegram fac
     )
 ```
 
-After that, run the vcommand above to publish the Telegram config file, you must to provide you Telegram Bot credentials and chat room information. 
+After that, run the command above to publish the Telegram config file, you must provide your Telegram Bot credentials and chat room information. 
+## Publishing the package
+Now, you should publish the package to generate the config file, after that, edit the config file with your Telegram Bot credentials.
 ### Laravel 4.2
+The config file will be generate here: ```app/config/packages/ricardofontanelli/laravel-telegram/config.php```
 ```php 
 php artisan config:publish ricardofontanelli/laravel-telegram
 ```
 ### Laravel 5.*
+The config file will be generate here: ```app/config/telegram.php```
 ```php 
 php artisan vendor:publish --provider="RicardoFontanelli\LaravelTelegram\TelegramServiceProvider"
 ```
+### Send a message:
 Now you can use it by ```php artisan tinker``` and run: 
 
-```php 
-Telegram::async()->sendMessage('default', 'Test message');
+```php
+// Send a message
+Telegram::sendMessage('default', 'Here we go!');
+// or async
+Telegram::async()->sendMessage('default', 'Here we go!');
 ```
+The first value is the config key name of the chat/group id where the Bot will publicate the message, you can provide the chat/group id directly. 
+### Get information about the Bot:
+```php
+// Send an async message
+Telegram::getMe()->getResult();
+```
+
+### Get Bot update:
+```php
+// Send an async message
+Telegram::getUpdates()->getResult();
+```
+
+### Other methods:
+The class has use a magic method to call unsuported methods:
+```php
+$params = ['method'=>'GET'];
+Telegram::getWebhookInfo($params)->getResult()
+```
+You can use the variable ```$params``` to send query parameters and define the HTTP method (according the Telegram Bot Api documentation).
+
+## Find more:
+You can find more here (Telegram Bot API)[https://core.telegram.org/bots/api], but if you need to call a method that the class doesn't support, feel free to send a PR.
--- src/RicardoFontanelli/LaravelTelegram/TelegramServiceProvider.php
+++ src/RicardoFontanelli/LaravelTelegram/TelegramServiceProvider.php
@@ -2,87 +2,330 @@
 
 namespace RicardoFontanelli\LaravelTelegram;
 
-use Illuminate\Support\ServiceProvider;
-use RicardoFontanelli\LaravelTelegram\Telegram;
-
-class TelegramServiceProvider extends ServiceProvider
+/**
+ * Service class to comunicate with Telegram API.
+ *
+ * @author Ricardo Fontanelli <ricardo.fontanelli@hotmail.com>
+ */
+class Telegram
 {
+    /**
+     * @var string an array with the main url and resources endpoint see provided from \Config::get('app.api_ftd')
+    */
+    private $endpoint = 'https://api.telegram.org/';
 
     /**
-     * Abstract type to bind Sentry as in the Service Container.
-     *
-     * @var string
+     * @var array a list of resources that are supported by this class, you can explore others resources using the __call method
+    */
+    private $resources = [
+        'sendMessage',
+        'getUpdates',
+        'getMe',
+    ];
+
+    private $botName;
+
+    private $token;
+
+    /**
+     * @var array a list of configured chat room list 'name' => telegram_chat_id
      */
-    public static $abstract = 'telegram';
+    private $chatList = [];
+
+    /**
+     * @var resource curl instance
+     */
+    private $client;
+
+    /**
+     * @var array HTTP body response converted to array
+     */
+    private $result;
+
+    private $httpStatus;
+
+    /**
+     * @var bool Define if the request will be async
+     */
+    private $async = false;
+
+    /**
+     * @var bool true or false if the request return an error
+     */
+    private $hasError;
+
+    /**
+     * Create a telegram Http Client.
+     *
+     * @param string $token   the bot Telegram token more in: https://core.telegram.org/bots
+     * @param string $botName the bot username
+     *
+     * @return object $this
+     */
+    public function __construct($token, $botName)
+    {
+        isset($token) ? $this->token = $token : null;
+        isset($botName) ? $this->botName = $botName : null;
+        $this->resetConf();
+
+        return $this;
+    }
     
     /**
-     * Indicates if loading of the provider is deferred.
+     * Define a list of chat room that the bot can send messages.
      *
-     * @var bool
-     */
-    protected $defer = false;
-
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-    */
-	public function boot()
-	{
-        // Laravel 4.x compatibility
-        if (version_compare($this->app::VERSION, '5.0') < 0) {
-            $this->package('ricardofontanelli/telegram', static::$abstract);
-        } else {
-            // the default configuration file
-            $this->publishes([
-                __DIR__ . '/../../config/config.php' => config_path(static::$abstract . '.php'),
-            ], 'config');
-        }
-	}
-
-    /**
-     * Register the service provider.
+     * @param array $list  a list of chat room ids that the Bot can send messages (name => telegram_group_id)
      *
-     * @return void
-     */
-    public function register()
+     * @return object $this
+     */ 
+    public function setChatList(array $list)
     {
-        $this->app->singleton(static::$abstract . '.config', function ($app) {
-            // sentry::config is Laravel 4.x
-            $user_config = $app['config'][static::$abstract] ?: $app['config'][static::$abstract . '::config'];
-            // Make sure we don't crash when we did not publish the config file
-            if (is_null($user_config)) {
-                $user_config = [];
-            }
-            return $user_config;
-        });
-        
-        
-        $this->app->bind('RicardoFontanelli\LaravelTelegram\Telegram', function ($app) {
-            
-            $user_config = $app[static::$abstract . '.config'];
+        $this->chatList = $list;
+        return $this;
+    }
+    
+    public function getResult()
+    {
+        return $this->result;
+    }
 
-            $token          = isset($user_config['token'])       ? $user_config['token']       : null;
-            $botusername    = isset($user_config['botusername']) ? $user_config['botusername'] : null;
-            $chats          = isset($user_config['chats'])       ? $user_config['chats']       : [];
+    public function getContent()
+    {
+        return $this->getResult();
+    }
 
-            $client = new Telegram($token, $botusername);
-            $client->setChatList($chats);
-            
-            return $client;
-        });
+    public function getResponse()
+    {
+        return $this->getResult();
+    }
 
-        $this->app->singleton(static::$abstract, 'RicardoFontanelli\LaravelTelegram\Telegram');
-        
+    public function getStatusCode()
+    {
+        return $this->httpStatus;
     }
 
     /**
-     * Get the services provided by the provider.
+     * Check has error in the last API call.
      *
-     * @return array
+     * @return bool
      */
-    public function provides()
+    public function hasError()
     {
-        return [static::$abstract];
+        return $this->hasError;
+    }
+
+    /**
+     * Define if the call should be sync or async.
+     *
+     * @param bool $status change the behavior of api call
+     *
+     * @return object $this
+     */
+    public function async($status = true)
+    {
+        $this->async = $status;
+
+        return $this;
+    }
+
+    /**
+     * A simple method for testing your bot's auth token. Requires no parameters. Returns basic information about the bot
+     *
+     * @return object $this
+     */
+    public function getMe()
+    {
+        return $this->callAPI('GET', 'getMe', []);
+    }
+
+    /**
+     * Send a message to a specific chat in Telegram.
+     *
+     * @param string $chatId    the key value of the chat config list. If you provide a concrete chat id (that isn't a key value of the config file), it will be used 
+     * @param string $text      a message with maximun lenght of 406 characters
+     * @param string $parseMode HTML or Markdown Telegram will parse characteres
+     *
+     * @return object $this
+     */
+    public function sendMessage($chatId, $text, $parseMode = 'HTML')
+    {
+        $params = [];
+
+        if (isset($chatId) && isset($text)) {
+            // Define the chat id, by config or concrete value 
+            if (isset($this->chatList[$chatId])) {
+                $chatId = $this->chatList[$chatId];
+            }
+            
+            $stringLenUtf8 = mb_strlen($text, 'UTF-8');
+
+            if ($stringLenUtf8 > 4096) {
+                $text = mb_substr($text, 0, 4096);
+            }
+
+            $params = [
+                    'chat_id'   => $chatId,
+                    'text'      => $text,
+                    'parse_mode'=> $parseMode,
+            ];
+            return $this->callAPI('POST', 'sendMessage', $params);
+        }
+        $this->result = ['ok' => false, 'result' => 'Invalid params'];
+
+        return $this;
+    }
+
+    /**
+     * Receive incoming updates
+     *
+     * @param int $offset dentifier of the first update to be returned
+     * @param int $limit  Limits the number of updates to be retrieved.
+     * @param int $timeout Timeout in seconds for long polling.
+     *
+     * @return object $this
+     */
+    public function getUpdates($offset = 0, $limit = 100, $timeout = 0)
+    {
+        $params = [];
+        
+        if (isset($offset)) {
+            $params['offset'] = $offset;
+        }
+
+        if (isset($limit)) {
+            $params['limit'] = $limit;
+        }
+
+        $params['timeout'] = $timeout;
+
+        return $this->callAPI('POST', 'getUpdates', $params);
+    }
+
+    /**
+     * Receive incoming updates
+     *
+     * @param int $name         the name of the method called
+     * @param int $arguments    an array with parameters to be send
+     *
+     * @return object $this
+     */
+    public static function __callStatic($name, $arguments)
+    {
+        (!isset($this->resources[$name])) ? $this->resources[] = $name : null;
+
+        $method = isset($arguments['method']) ? $arguments['method'] : 'POST';
+        unset($arguments['method']);
+        
+        return $this->callAPI('POST', $name, ($arguments));
+    }
+
+    /**
+     * Receive incoming updates
+     *
+     * @param int $name         the name of the method called
+     * @param int $arguments    an array with parameters to be send
+     *
+     * @return object $this
+     */
+    public function __call($name, $arguments)
+    {
+        $this->resources[] = $name;
+        $method = isset($arguments['method']) ? $arguments['method'] : 'POST';
+        unset($arguments['method']);
+        
+        return $this->callAPI('POST', $name, ($arguments));
+    }
+
+    /**
+     * Generate the resource URL based on the transaction type.
+     *
+     * @param string $name resouce name, based on $resources
+     *
+     * @throws Exception if the resource name is invalid
+     */
+    private function generateResourceUrl($name)
+    {
+        if (in_array($name, $this->resources)) {
+            return $this->endpoint . 'bot'. $this->token . '/' . $name;
+        }
+        throw new \Exception('Invalid Telegram resource name' . $name, 1);
+    }
+
+    /** Reset the class property structure for a next http call */
+    private function resetConf()
+    {
+        $this->client = null;
+        $this->result = null;
+        $this->httpStatus = 500;
+        $this->hasError = true;
+    }
+
+    /** Setup default configuration for the curl client */
+    private function setupClient()
+    {
+        $this->resetConf();
+        $this->client = curl_init();
+        curl_setopt($this->client, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($this->client, CURLOPT_ENCODING, 'gzip');
+        curl_setopt($this->client, CURLOPT_VERBOSE, false);
+        curl_setopt($this->client, CURLOPT_HEADER, false);
+        curl_setopt($this->client, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+        if ($this->async) {
+            curl_setopt($this->client, CURLOPT_FRESH_CONNECT, true);
+            curl_setopt($this->client, CURLOPT_TIMEOUT, 1);
+        }
+    }
+
+    /**
+     * Send a HTTP request to que API endpoint, it will register an erro (hasError) if the remote server return HTTP error (400,500,...) or 200 with json error structure array('ok'=>false).
+     *
+     * @param string $method       HTTP method (POST, GET ...)
+     * @param string $resourceName resouce name, based on $resources
+     * @param array  $data         an array of data to be converted to JSON and sent to the API
+     *
+     * @return $this
+     */
+    private function callAPI($method, $resourceName, $data)
+    {
+        $this->setupClient();
+        $url = $this->generateResourceUrl($resourceName);
+        $dataString = json_encode($data);
+
+        if ($method == 'POST') {
+            curl_setopt($this->client, CURLOPT_CUSTOMREQUEST, 'POST');
+            if ($data) {
+                curl_setopt($this->client, CURLOPT_POSTFIELDS, $dataString);
+                curl_setopt($this->client, CURLOPT_HTTPHEADER, ['Content-Type: application/json', 'Content-Length: '.strlen($dataString)]);
+            }
+        }
+
+        curl_setopt($this->client, CURLOPT_URL, $url);
+
+        try {
+            $this->result = curl_exec($this->client);
+            $this->httpStatus = curl_getinfo($this->client, CURLINFO_HTTP_CODE);
+            curl_close($this->client);
+
+            if (!$this->async) {
+                if ($this->httpStatus == '200') {
+                    $this->result = json_decode($this->result, true);
+                    if (is_array($this->result)) {
+                        if (array_key_exists('ok', $this->result)) {
+                            if ($this->result['ok'] == true) {
+                                $this->hasError = false;
+                            }
+                        }
+                    }
+                }
+            } else {
+                $this->hasError = false;
+                $this->result = [];
+            }
+        } catch (\Exception $e) {
+            $this->httpStatus = '500';
+            $this->result = $e->getMessage().' - '.$url;
+        }
+
+        return $this;
     }
 }
